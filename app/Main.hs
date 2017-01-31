5b4bd532e4bd677d758c874819fefca4203b0063
John Ky <newhoggy@gmail.com>
2017-01-31 21:11:11 +1100

    Main

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,24 @@
 module Main where
 
+import HaskellWorks.Jq.Parser
 import System.Environment
+import Text.Parsec
+
+data Opts = Opts
+  {
+  }
+
+defaultOpts :: Opts
+defaultOpts = Opts {}
+
+runOpts :: Opts -> [String] -> IO ()
+runOpts opts (x:xs) = do
+  let expr = parse jqSelector "" x
+  print expr
+  runOpts opts xs
+runOpts _ [] = return ()
 
 main :: IO ()
 main = do
   args <- getArgs
-  print args
+  runOpts defaultOpts args
